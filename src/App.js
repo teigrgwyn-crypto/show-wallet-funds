6105251cd1f1ae2bfebfa06c89ce4fc6aed69db7
Cory Young <teigrgwyn@gmail.com>
2022-01-09 15:29:19 -0600

    setup & pruning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import React from 'react-router-dom';
+
+// import { LCDClient, Coin } from '@terra-money/terra.js';
+// import { common } from 'protobufjs';
+
+import './App.scss';
 
 function App() {
+  const [query, setQuery] = useState({ address: "" });
+
+  // const terra = new LCDClient({
+  //   URL: 'https://bombay-lcd.terra.dev',
+  //   chainID: 'bombay-12',
+  // });
+
+  // const terra = new LCDClient({
+  //   URL: 'http://localhost:1317',
+  //   chainID: 'localterra'
+  // });
+
+  // const offerCoin = new Coin('uusd', '1000000');
+  // terra.market.swapRate(offerCoin, 'ukrw').then(c => {
+  //   console.log(`${offerCoin.toString()} can be swapped for ${c.toString()}`);
+  // });
+
+  const onQueryChange = e => {
+    setQuery({ ...query, [e.target.name]: e.target.value });
+  }
+
+  const onQuerySubmit = e => {
+    setQuery({ ...query, [e.target.name]: "" });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='app'>
+      <form onSubmit={ onQuerySubmit }>
+        <input
+          name='address'
+          value={ query.address }
+          onChange={ onQueryChange }
+          placeholder='address'
+          spellCheck='false'
+          autoComplete='off'
+        />
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
+
+/*
+  Theme:
+    Bluish: #20232A
+    Dark Bluish: #0D1117
+    Dark Gray: #181A1B
+    Near Black: #0D0D0D
+*/
